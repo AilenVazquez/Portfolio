0b9a7bf1cddd86dfef5003edb2be6d2c87ed4f97
ailenvazquez <ailenvazquez13.av@gmail.com>
2022-10-22 21:50:46 -0300

    Creacion de header y bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BarraComponent } from './components/header/barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
